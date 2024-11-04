f1e89b68bfb438b75748e8c5f67a99023382ebd8
George Busby <georgebusby08@gmail.com>
2024-11-04 15:22:48 -0700

    Update tech.py

--- tech.py
+++ tech.py
@@ -1,1 +1,21 @@
-
+class Tech:
+  def __init__(self,name,storage):
+    self.name = name
+    self.storage =  storage
+class Phone(Tech):
+  def __init__(self,name,storage,color):
+    self.super().__init__(name,storage)
+    self.color=color
+  def __str__(self):
+    return str(f"A {self.color} {self.name} with {str(self.storage)}GB of storage.")
+  def __repr__(self):
+    return f"Phone({self.name},{str(self.storage)},{self.color})"
+class Laptop(Tech):
+  def __init__(self,name,storage,size):
+    self.super().__init__(name,storage)
+    self.size = int(size)
+  def __str__(self):
+    return str(f'{str(self.size)}-inch {self.name} with {str(self.storage)}GB of storage.')
+  def __repr__(self):
+    return f"Laptop({self.name},{str(self.storage)},{str(self.size)})"
+    
