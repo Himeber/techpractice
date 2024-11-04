25a6b01effe23094c85163c1423d3636d8f24ffc
George Busby <georgebusby08@gmail.com>
2024-11-04 15:25:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from tech import *
 
 phon = Phone("BearPhone",392,"red")
 compy = Laptop("Lappy486",8,12)
+
 print(phon)
 print(compy)
+print("-----------------------------------)
 print(phon.__repr__())
 print(compy.__repr__())
